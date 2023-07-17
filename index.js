a9d3d274ad01fe2cd14ff161abfee0c7dcf918d6
martinaemunoz <martinaemunoz@gmail.com>
2023-07-17 19:02:10 -0400

    initial tests

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
+import fs from 'fs';
+
+export const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    // Identifica si la ruta existe.
+    if (fs.existsSync(path)) {
+
+    } else {
+      // Si no existe la ruta se rechaza la promesa.
+      reject("La ruta no existe");
+    }
+  });
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,15 @@
-const mdLinks = require('../');
-
+import { mdLinks } from '../index.js';
 
 describe('mdLinks', () => {
-
   it('should...', () => {
-    console.log('FIX ME!');
+    console.log("FIX ME!");
   });
-
+  /*it("Debería devolver una promesa", () => {
+    expect(mdLinks()).toBe(typeof Promise);
+  });*/
+  it('Debe rechazar la promesa cuando el path no existe', () => {
+    return mdLinks('/marti/noexiste.md').catch((error) => {
+      expect(error).toBe('La ruta no existe');
+    })
+  });
 });
